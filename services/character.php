36372b5e581008d64a44fe91e372fda332f1355c
MystixCode <mystixrage@gmx.ch>
2020-10-04 15:59:16 +0200

    character get, edit

--- services/character.php
+++ services/character.php
@@ -24,8 +24,30 @@ class Character {
     }
 
     ## GET #####################################################################
-    public function get($data) {
-        return "api todo get characterdata from db and return data";
+    public function get($data, $user_id) {
+        $character_id=$data->character_id;
+        if (isset($user_id) && isset($character_id)) {
+            if ((is_valid('numeric', $user_id) == true) && (is_valid('numeric', $character_id) == true)) {
+                $pdo = new DB();
+                $pdo = $pdo->connect();
+                $stmt = $pdo->prepare('SELECT id, name FROM chars WHERE id = :character_id AND user_id = :user_id');
+                $stmt->execute(array($character_id, $user_id));
+
+                $payload = array();
+                $result = $stmt->fetch();
+                $payload['character_id']= $result['id'];
+                $payload['character_name']= $result['name'];
+
+                return  json_encode($payload);
+
+
+                //return json_encode(array(character_name => $chars, message => 'DONE TODO..'));
+            }
+        }
+
+        return json_encode(array(character_name => 'error', message => 'TODO API character>get'));
+
+
     }
 
     ## Create ##################################################################
@@ -46,7 +68,20 @@ class Character {
     ## EDIT ####################################################################
     public function edit($data, $user_id) {
         //TODO if user_id correct  for character_id
-        return "api todo edit chararcter";
+
+
+        $character_id=$data->character_id;
+        $character_name=$data->character_name;
+        if (isset($user_id) && isset($character_id) && isset($character_name) ) {
+            if ((is_valid('numeric', $user_id) == true) && (is_valid('numeric', $character_id) == true) && (is_valid('alphanumeric_s3', $character_name) == true) ) {
+                $pdo = new DB();
+                $pdo = $pdo->connect();
+                $stmt = $pdo->prepare('UPDATE chars SET name = :character_name WHERE id = :character_id AND user_id = :user_id');
+                $stmt->execute(array($character_name, $character_id, $user_id));
+                return json_encode(array(character_name => 'testcharname', message => 'DONE TODO..'));
+            }
+        }
+        return json_encode("api error character>edit");
     }
 
     ## DELETE ##################################################################
